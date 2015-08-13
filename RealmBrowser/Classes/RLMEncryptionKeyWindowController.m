e6f4c6afd32639ee64d830d377c577f69b9f9d45
Tim Oliver <me@timoliver.com.au>
2015-08-13 16:49:39 +0800

    Updated UI and added hexadecimal conversion logic.

--- RealmBrowser/Classes/RLMEncryptionKeyWindowController.m
+++ RealmBrowser/Classes/RLMEncryptionKeyWindowController.m
@@ -16,11 +16,16 @@
 //
 ////////////////////////////////////////////////////////////////////////////
 
+@import Realm;
+
 #import "RLMEncryptionKeyWindowController.h"
 
-@interface RLMEncryptionKeyWindowController ()
+@interface RLMEncryptionKeyWindowController () <NSTextFieldDelegate>
 
 @property (nonatomic, strong) NSURL *realmFilePath;
+
+- (BOOL)testRealmFileWithEncryptionKey:(NSString *)key;
+- (NSData *)dataFromHexadecimalString:(NSString *)string;
 
 @end
 
@@ -35,19 +40,62 @@
     return self;
 }
 
-- (void)windowDidLoad {
-    [super windowDidLoad];
+- (void)controlTextDidChange:(NSNotification *)notification {
+    NSString *stringValue = self.keyTextField.stringValue;
     
+    //Ensure only hex-compatible characters have been entered
+    NSCharacterSet *chars = [[NSCharacterSet characterSetWithCharactersInString:@"0123456789ABCDEFabcdef"] invertedSet];
+    BOOL isValid = (NSNotFound == [stringValue rangeOfCharacterFromSet:chars].location) && stringValue.length == 128;
+    self.okayButton.enabled = isValid;
 }
 
 - (IBAction)okayButtonClicked:(id)sender
 {
+    if ([self testRealmFileWithEncryptionKey:self.keyTextField.stringValue] == NO) {
+        self.errorTextField.hidden = NO;
+        return;
+    }
+    
     [self.window.sheetParent endSheet:self.window returnCode:NSModalResponseOK];
 }
 
 - (IBAction)cancelButtonClicked:(id)sender
 {
     [self.window.sheetParent endSheet:self.window returnCode:NSModalResponseCancel];
+}
+
+#pragma mark - Encryption Testing -
+- (BOOL)testRealmFileWithEncryptionKey:(NSString *)key
+{
+    NSData *encryptionKeyData = [self dataFromHexadecimalString:key];
+    
+    NSError *error = nil;
+    [RLMRealm realmWithPath:self.realmFilePath.path encryptionKey:encryptionKeyData readOnly:YES error:&error];
+    if (error)
+        return NO;
+    
+    return YES;
+}
+
+// http://stackoverflow.com/a/13627835/599344
+- (NSData *)dataFromHexadecimalString:(NSString *)string
+{
+    string = [string lowercaseString];
+    NSMutableData *data= [NSMutableData new];
+    unsigned char whole_byte;
+    char byte_chars[3] = {'\0','\0','\0'};
+    int i = 0;
+    NSInteger length = string.length;
+    while (i < length-1) {
+        char c = [string characterAtIndex:i++];
+        if (c < '0' || (c > '9' && c < 'a') || c > 'f')
+            continue;
+        byte_chars[0] = c;
+        byte_chars[1] = [string characterAtIndex:i++];
+        whole_byte = strtol(byte_chars, NULL, 16);
+        [data appendBytes:&whole_byte length:1];
+    }
+    return data;
 }
 
 @end
