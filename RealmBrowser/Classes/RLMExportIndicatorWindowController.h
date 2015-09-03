aa2ea72fc28c5a96b8df5ec59292934ad8100986
Tim Oliver <me@timoliver.com.au>
2015-09-03 12:57:30 +0800

    Created UI elements for progress indicator.

---
+++ RealmBrowser/Classes/RLMExportIndicatorWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RLMExportIndicatorWindowController.h
+//  
+//
+//  Created by Tim Oliver on 9/3/15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RLMExportIndicatorWindowController : NSWindowController
+
+@end
---
+++ RealmBrowser/Classes/RLMExportIndicatorWindowController.m
@@ -0,0 +1,23 @@
+//
+//  RLMExportIndicatorWindowController.m
+//  
+//
+//  Created by Tim Oliver on 9/3/15.
+//
+//
+
+#import "RLMExportIndicatorWindowController.h"
+
+@interface RLMExportIndicatorWindowController ()
+
+@end
+
+@implementation RLMExportIndicatorWindowController
+
+- (void)windowDidLoad {
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
