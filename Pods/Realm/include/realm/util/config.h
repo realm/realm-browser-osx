089c7c62b8ee2e2a66b02cdfa962347b5a070e6f
Tim Oliver <me@timoliver.com.au>
2015-09-25 17:36:30 +0800

    Updated to Realm 0.95.2

--- Pods/Realm/include/realm/util/config.h
+++ Pods/Realm/include/realm/util/config.h
@@ -6,14 +6,14 @@
  *
  *************************************************************************/
 
-#define REALM_VERSION "unknown"
+#define REALM_VERSION "0.92.2"
 
-#define REALM_INSTALL_PREFIX      "/Users/realm/workspace/core_osx/install"
-#define REALM_INSTALL_EXEC_PREFIX "/Users/realm/workspace/core_osx/install"
-#define REALM_INSTALL_INCLUDEDIR  "/Users/realm/workspace/core_osx/install/include"
-#define REALM_INSTALL_BINDIR      "/Users/realm/workspace/core_osx/install/bin"
-#define REALM_INSTALL_LIBDIR      "/Users/realm/workspace/core_osx/install/lib"
-#define REALM_INSTALL_LIBEXECDIR  "/Users/realm/workspace/core_osx/install/libexec"
+#define REALM_INSTALL_PREFIX      "/usr/local"
+#define REALM_INSTALL_EXEC_PREFIX "/usr/local"
+#define REALM_INSTALL_INCLUDEDIR  "/usr/local/include"
+#define REALM_INSTALL_BINDIR      "/usr/local/bin"
+#define REALM_INSTALL_LIBDIR      "/usr/local/lib"
+#define REALM_INSTALL_LIBEXECDIR  "/usr/local/libexec"
 
 #ifdef REALM_DEBUG
 #  define REALM_MAX_BPNODE_SIZE 1000
