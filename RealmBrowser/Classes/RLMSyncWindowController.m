975cf556f35325c6b0116f7a9fa2a4d6aadaaeb9
Tim Oliver <me@timoliver.com.au>
2015-12-01 19:38:18 +0800

    Added sync credentials window controller

---
+++ RealmBrowser/Classes/RLMSyncWindowController.m
@@ -0,0 +1,13 @@
+//
+//  RLMSyncWindowController.m
+//  RealmBrowser
+//
+//  Created by Tim Oliver on 1/12/2015.
+//  Copyright © 2015 Realm inc. All rights reserved.
+//
+
+#import "RLMSyncWindowController.h"
+
+@implementation RLMSyncWindowController
+
+@end
