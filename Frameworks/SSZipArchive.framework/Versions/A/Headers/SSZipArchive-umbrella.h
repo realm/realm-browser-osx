a4c1b3164c6aff6e226cbbbed4b97d5679c3ee58
Tim Oliver <me@timoliver.com.au>
2016-02-23 23:15:51 +0800

    Removed older copies of frameworks.

--- Frameworks/SSZipArchive.framework/Versions/A/Headers/SSZipArchive-umbrella.h
+++
@@ -1,8 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-#import "Common.h"
-#import "SSZipArchive.h"
-
-FOUNDATION_EXPORT double SSZipArchiveVersionNumber;
-FOUNDATION_EXPORT const unsigned char SSZipArchiveVersionString[];
-
--- Frameworks/SpreadsheetWriter.framework/Versions/A/Headers/SpreadsheetWriter-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-#import "SpreadsheetWriter.h"
-
-FOUNDATION_EXPORT double SpreadsheetWriterVersionNumber;
-FOUNDATION_EXPORT const unsigned char SpreadsheetWriterVersionString[];
-
