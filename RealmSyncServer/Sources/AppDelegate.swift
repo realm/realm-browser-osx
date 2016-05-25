89cde2104cac6e8284fa9e17201d12666a69a526
Dmitry Obukhov <stel2k@gmail.com>
2016-05-25 12:24:28 +0200

    add the project for RealmSyncServer

---
+++ RealmSyncServer/Sources/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RealmSyncServer
+//
+//  Created by Dmitry Obukhov on 5/25/16.
+//  Copyright © 2016 Realm Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RealmSyncServer/Sources/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RealmSyncServer
+//
+//  Created by Dmitry Obukhov on 5/25/16.
+//  Copyright © 2016 Realm Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
