9a062dec6c21877fd46fae7ea8ed1f88d936b228
Tim Oliver <me@timoliver.com.au>
2016-05-27 16:42:49 +0800

    Moved constants to dedicated file-set.

---
+++ RealmBrowser/Support/RLMBrowserConstants.h
@@ -0,0 +1,31 @@
+////////////////////////////////////////////////////////////////////////////
+//
+// Copyright 2014-2015 Realm Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+////////////////////////////////////////////////////////////////////////////
+
+#import <Foundation/Foundation.h>
+
+extern const NSUInteger kTopTipDelay;
+extern const NSUInteger kMaxFilesPerCategory;
+extern const NSInteger kMaxNumberOfFilesAtOnce;
+extern const CGFloat kMenuImageSize ;
+
+extern NSString * const kRealmFileExtension;
+extern NSString * const kDeveloperFolder;
+extern NSString * const kSimulatorFolder;
+extern NSString * const kDesktopFolder;
+extern NSString * const kDownloadFolder;
+extern NSString * const kDocumentsFolder;
