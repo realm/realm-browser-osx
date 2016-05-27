1192482ad046dd6bbd98bc690a9c8947d5ac3daa
Tim Oliver <me@timoliver.com.au>
2016-05-27 16:53:41 +0800

    Moved constants file.

--- RealmBrowser/Models/RLMBrowserConstants.m
+++
@@ -1,9 +0,0 @@
-//
-//  RLMBrowserConstants.m
-//  RealmBrowser
-//
-//  Created by Tim Oliver on 26/05/2016.
-//  Copyright © 2016 Realm inc. All rights reserved.
-//
-
-#import "RLMBrowserConstants.h"
