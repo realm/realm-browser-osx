f5eb1b6d9eb93bf17c0a224be2e7992f078b08dc
Tim Oliver <me@timoliver.com.au>
2016-06-02 16:48:45 +0800

    Reverted case of non-optional Bool values.

--- RealmBrowser/Views/RLMOptionalBoolTableCellView.m
+++ RealmBrowser/Views/RLMOptionalBoolTableCellView.m
@@ -38,7 +38,7 @@
     
     NSPopUpButton *popupButton = [[NSPopUpButton alloc] initWithFrame:self.bounds pullsDown:NO];
     popupButton.translatesAutoresizingMaskIntoConstraints = NO;
-    [popupButton addItemsWithTitles:@[@"nil", @"false", @"true"]];
+    [popupButton addItemsWithTitles:@[@"nil", @"False", @"True"]];
     popupButton.bordered = NO;
     self.popupControl = popupButton;
     [self addSubview:popupButton];
