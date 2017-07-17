f6a9fab12ff55d1eaa6d7c49deeea920dbb6eed6
Dmitry Obukhov <stel2k@gmail.com>
2017-07-17 11:48:50 +0300

    Handle uncaught exception when open non existing file via NSOpenPanel

--- RealmBrowser/Controllers/RLMDocumentController.m
+++ RealmBrowser/Controllers/RLMDocumentController.m
@@ -58,4 +58,17 @@
     }
 }
 
+- (void)openDocument:(id)sender {
+    @try {
+        [super openDocument:sender];
+    } @catch (NSException *exception) {
+        // NSOpenPanel in sandboxed environment doesn't handle changes in path and crashes if one of the parent directories has been renamed.
+        // This case usually happens when users try to open realm file from simulator's directory while Xcode launches an app and changes
+        // simulator root path.
+        // See https://rink.hockeyapp.net/manage/apps/405793/app_versions/27/crash_reasons/176155071.
+        NSAlert *alert = [NSAlert alertWithMessageText:@"Failed to open realm file" defaultButton:nil alternateButton:nil otherButton:nil informativeTextWithFormat:@"File has been moved to a different location, please try to open it again."];
+        [alert runModal];
+    }
+}
+
 @end
